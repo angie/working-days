7b66d349128d1846bb2a076f3282b99ccb6e7230
Angie Merryweather <6738119+angie@users.noreply.github.com>
2022-06-20 20:55:45 +0100

    style: remove funky font for now

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -11,9 +11,7 @@ export default function Index() {
           <div className="relative sm:overflow-hidden">
             <div className="lg:pb-18 relative px-4 pt-16 pb-8 sm:px-6 sm:pt-24 sm:pb-14 lg:px-8 lg:pt-32">
               <h1 className="text-center text-6xl font-extrabold tracking-tight sm:text-8xl lg:text-9xl">
-                <span className="block font-pa-chrome uppercase text-rose-500">
-                  working days
-                </span>
+                <span className="block text-rose-500">working days</span>
               </h1>
               <div className="mx-auto mt-10 max-w-sm sm:flex sm:max-w-none sm:justify-center">
                 {user ? (
