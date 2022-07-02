a884d45c593b2f3d5d7362b69c7076585ef35501
Angie Merryweather <6738119+angie@users.noreply.github.com>
2022-07-02 10:26:54 +0100

    style(colours): add some happy hues

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -5,13 +5,13 @@ import { useOptionalUser } from "~/utils";
 export default function Index() {
   const user = useOptionalUser();
   return (
-    <main className="relative min-h-screen bg-black sm:flex sm:items-center sm:justify-center">
+    <main className="relative min-h-screen bg-solitaire-100 sm:flex sm:items-center sm:justify-center">
       <div className="relative sm:pb-16 sm:pt-8">
         <div className="mx-auto max-w-7xl sm:px-6 lg:px-8">
           <div className="relative sm:overflow-hidden">
             <div className="lg:pb-18 relative px-4 pt-16 pb-8 sm:px-6 sm:pt-24 sm:pb-14 lg:px-8 lg:pt-32">
               <h1 className="text-center text-6xl font-extrabold tracking-tight sm:text-8xl lg:text-9xl">
-                <span className="block text-rose-500">working days</span>
+                <span className="block text-prussian-blue">working days</span>
               </h1>
               <div className="mx-auto mt-10 max-w-sm sm:flex sm:max-w-none sm:justify-center">
                 {user ? (
@@ -25,13 +25,13 @@ export default function Index() {
                   <div className="space-y-4 sm:mx-auto sm:inline-grid sm:grid-cols-2 sm:gap-5 sm:space-y-0">
                     <Link
                       to="/join"
-                      className="flex items-center justify-center rounded-md border border-transparent bg-white px-4 py-3 text-base font-medium text-rose-700 shadow-sm hover:bg-rose-50 sm:px-8"
+                      className="flex items-center justify-center rounded-md border border-transparent bg-white px-4 py-3 text-base font-medium text-prussian-blue shadow-sm hover:bg-rose-50 sm:px-8"
                     >
                       Sign up
                     </Link>
                     <Link
                       to="/login"
-                      className="flex items-center justify-center rounded-md bg-rose-500 px-4 py-3 font-medium text-white hover:bg-rose-600 "
+                      className="flex items-center justify-center rounded-md bg-persian-pink-300 px-4 py-3 font-medium text-prussian-blue hover:bg-persian-pink-200"
                     >
                       Log In
                     </Link>
