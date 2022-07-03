6d89d4d4f2e216201dbcb3c4d2bde77a3c6214cf
Angie Merryweather <6738119+angie@users.noreply.github.com>
2022-07-03 09:27:36 +0100

    task: add util to return working days in a given month

--- app/date-utils.test.ts
+++ app/date-utils.test.ts
@@ -1,8 +1,43 @@
-import { getToday } from "./date-utils";
+import { getToday, getWorkingDays } from "./date-utils";
 
 test("getToday should return today's date at midnight", () => {
   const today = new Date().toJSON().split("T")[0];
   const todayMidnight = new Date(today + "T00:00:00.000Z");
 
   expect(getToday()).toEqual(todayMidnight);
+});
+
+test("getWorkingDays should return all working days in a month", () => {
+  const month = new Date("2019-01-01");
+  const workingDays = [
+    new Date("2019-01-01"),
+    new Date("2019-01-02"),
+    new Date("2019-01-03"),
+    new Date("2019-01-04"),
+    // skip Saturday 5th and Sunday 6th
+    new Date("2019-01-07"),
+    new Date("2019-01-08"),
+    new Date("2019-01-09"),
+    new Date("2019-01-10"),
+    new Date("2019-01-11"),
+    // skip Saturday 12th and Sunday 13th
+    new Date("2019-01-14"),
+    new Date("2019-01-15"),
+    new Date("2019-01-16"),
+    new Date("2019-01-17"),
+    new Date("2019-01-18"),
+    // skip Saturday 19th and Sunday 20th
+    new Date("2019-01-21"),
+    new Date("2019-01-22"),
+    new Date("2019-01-23"),
+    new Date("2019-01-24"),
+    new Date("2019-01-25"),
+    // skip Saturday 26th and Sunday 27th
+    new Date("2019-01-28"),
+    new Date("2019-01-29"),
+    new Date("2019-01-30"),
+    new Date("2019-01-31"),
+  ];
+
+  expect(getWorkingDays(month)).toEqual(workingDays);
 });
--- app/date-utils.ts
+++ app/date-utils.ts
@@ -1,7 +1,24 @@
 import dayjs from "dayjs";
 import utc from "dayjs/plugin/utc";
 
+dayjs.extend(utc);
+
 export const getToday = (): Date => {
-  dayjs.extend(utc);
   return dayjs.utc().startOf("day").toDate();
+};
+
+export const getWorkingDays = (month: Date) => {
+  const workingDays = [];
+  const startDate = dayjs.utc(month).startOf("month");
+  const endDate = dayjs.utc(month).endOf("month");
+  for (
+    let date = startDate;
+    date.isBefore(endDate);
+    date = date.add(1, "day")
+  ) {
+    if (date.day() !== 0 && date.day() !== 6) {
+      workingDays.push(date.toDate());
+    }
+  }
+  return workingDays;
 };
